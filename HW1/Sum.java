1e4febd9a356e602f9faa0d410b76c28b1619133
Pavel Bezborodov <dfa.sro591@gmail.com>
2018-03-01 00:20:11 +0300

    Minor fixes in first HW

--- HW1/Sum.java
+++ HW1/Sum.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
+//right way is to do it by symbol, but you can also use this:
 public class Sum{
     public static void main (String[] args){
         int sum = 0;
         for (int i = 0; i<args.length; i++) {
-            args[i]=args[i].replaceAll("[^1234567890-]+"," ");
+            args[i]=args[i].replaceAll("\\p{javaWhitespace}+"," ");
             args[i]=args[i].trim();
             String[] a = args[i].split(" ");
             for (int j = 0; j<a.length; j++){
